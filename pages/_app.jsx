800967b394110dc530ea2396ac4dd15ebf0ab8c7
Obi Fortune <gabrielobi.of@gmail.com>
2022-03-20 14:26:27 +0200

    Updated the _app layout

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import Layout from "../components/Layout";
+import MetaHeader from "../components/MetaHeader";
+import "../styles/globals.scss";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Layout>
+        <MetaHeader />
+        <Component {...pageProps} />
+      </Layout>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
