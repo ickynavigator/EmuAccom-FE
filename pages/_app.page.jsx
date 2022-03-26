ec9d93da194b6e3e300e7db2815b0c60f6b5e9f8
Obi Fortune <gabrielobi.of@gmail.com>
2022-03-26 14:25:34 +0200

    added the global theme settings

--- pages/_app.page.jsx
+++ pages/_app.page.jsx
@@ -1,15 +1,19 @@
+import { MantineProvider } from "@mantine/core";
 import React from "react";
 import Layout from "../components/Layout";
 import MetaHeader from "../components/MetaHeader";
+import theme from "../config/mantine.config";
 import "../styles/globals.scss";
 
-function MyApp({ Component, pageProps }) {
+function App({ Component, pageProps }) {
   return (
-    <Layout>
+    <MantineProvider theme={theme}>
+      <Layout>
         <MetaHeader />
         <Component {...pageProps} />
       </Layout>
+    </MantineProvider>
   );
 }
 
-export default MyApp;
+export default App;
