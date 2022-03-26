aca78204a8c2ce8229cc6f7de1f7a934c4a44852
Obi Fortune <gabrielobi.of@gmail.com>
2022-03-26 14:10:08 +0200

    added the mantine config

--- config/mantine-config.js
+++
@@ -1,5 +0,0 @@
-export default {
-  // Override any other properties from default theme
-  fontFamily: "Open Sans, sans serif",
-  spacing: { xs: 15, sm: 20, md: 25, lg: 30, xl: 40 },
-};
---
+++ config/mantine.config.js
@@ -0,0 +1,5 @@
+export default {
+  // Override any other properties from default theme
+  fontFamily: `-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji"`,
+  spacing: { xs: 15, sm: 20, md: 25, lg: 30, xl: 40 },
+};
