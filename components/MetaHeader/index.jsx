72065c81137bacbfdc539bbc974a44343f0b4595
Obi Fortune <gabrielobi.of@gmail.com>
2022-03-28 01:42:50 +0300

    Fixed metaheader eslint issues

--- components/MetaHeader/index.jsx
+++ components/MetaHeader/index.jsx
@@ -1,12 +1,6 @@
 import Head from "next/head";
+import React from "react";
 
-export const init = {
-  title: "Emu Accommodation",
-  description:
-    "Emu Accommodation - Book your stay in EMU. A grad project built in ITEC403/404",
-  keywords:
-    "Emu Accommodation, Booking, Accommodation, EMU, ITEC, ITEC403, ITEC404, Graduation Project",
-};
 export const MetaHeader = ({
   title,
   description,
@@ -14,6 +8,13 @@ export const MetaHeader = ({
   keywords,
   children,
 }) => {
+  const init = {
+    title: "Emu Accommodation",
+    description:
+      "Emu Accommodation - Book your stay in EMU. A grad project built in ITEC403/404",
+    keywords:
+      "Emu Accommodation, Booking, Accommodation, EMU, ITEC, ITEC403, ITEC404, Graduation Project",
+  };
   return (
     <Head>
       <title>{title || init.title}</title>
