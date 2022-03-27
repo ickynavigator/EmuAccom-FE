4bff0cf271ddf36cc0b7a909f4bacc00f9b01d15
Obi Fortune <gabrielobi.of@gmail.com>
2022-03-28 01:04:31 +0300

    added proper footer positioning

--- components/Layout/index.jsx
+++ components/Layout/index.jsx
@@ -1,13 +1,17 @@
+import React from "react";
 import Footer from "../Footer";
 import Header from "../Header";
 
 export const Layout = ({ children }) => {
+  // TODO FIX THIS RETURN ISSUE
+  console.log("Layout");
+
   return (
-    <>
+    <div id="mainApplicationWrapper">
       <Header />
       {children}
       <Footer />
-    </>
+    </div>
   );
 };
 
