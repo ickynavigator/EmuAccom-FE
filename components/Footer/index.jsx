c985b857a54f4a1c70eb35933f346a6498dc6d46
Obi Fortune <gabrielobi.of@gmail.com>
2022-03-28 00:48:54 +0300

    footer tweaks

--- components/Footer/index.jsx
+++ components/Footer/index.jsx
@@ -1,18 +1,37 @@
+import { Button, Group, Navbar, Text } from "@mantine/core";
+import React from "react";
 import styles from "../../styles/components/Footer.module.scss";
 
 export const Footer = () => {
+  const year = new Date().getFullYear();
+
   return (
-    <>
-      <footer className={styles.footer}>
-        <a href="/" rel="noopener noreferrer">
-          Emu Accom
-        </a>
-        <a href="#" target="_blank" rel="noopener noreferrer">
-          {/* TODO add a github url here */}
-          Github source code
-        </a>
-      </footer>
-    </>
+    <Navbar component="footer" height={80} className={styles.footer}>
+      <Group sx={{ height: "100%" }} px={10} position="apart">
+        <Text>
+          EmuAccom - Group 5 <span>&copy; {year}</span>
+        </Text>
+
+        <Group>
+          <Button
+            component="a"
+            href="https://github.com/ickynavigator/EmuAccom-FE"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Frontend source code
+          </Button>
+          <Button
+            component="a"
+            href="https://github.com/ickynavigator/EmuAccom-BE"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Backend source code
+          </Button>
+        </Group>
+      </Group>
+    </Navbar>
   );
 };
 export default Footer;
