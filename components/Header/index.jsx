43ca5875c53670682bffbe9720a767a5ab8bfabc
Obi Fortune <gabrielobi.of@gmail.com>
2022-03-30 08:56:53 +0300

    lint issues

--- components/Header/index.jsx
+++ components/Header/index.jsx
@@ -1,23 +1,65 @@
 import {
   ActionIcon,
+  Anchor,
+  Burger,
   Button,
+  createStyles,
   Group,
   Header,
+  MediaQuery,
+  Navbar,
   Text,
-  useMantineColorScheme,
+  useMantineColorScheme
 } from "@mantine/core";
 import React from "react";
 import { MoonStars, Sun } from "tabler-icons-react";
 import { useAuth } from "../../hooks";
 
-export const Index = () => {
+const useStyles = createStyles(theme => ({
+  navbar: {
+    [theme.fn.largerThan("sm")]: {
+      display: "none",
+    },
+  },
+
+  links: {
+    [theme.fn.smallerThan("sm")]: {
+      display: "none",
+    },
+  },
+}));
+
+export const MenuList = () => {
+  const { isAuthenticated } = useAuth();
+
+  return (
+    <>
+      <Anchor href="/about">About</Anchor>
+      {isAuthenticated && <Anchor>protected</Anchor>}
+    </>
+  );
+};
+
+export const Index = ({ opened, setOpened }) => {
   const { isAuthenticated } = useAuth();
   const { colorScheme, toggleColorScheme } = useMantineColorScheme();
+  const { classes } = useStyles();
 
   return (
     <Header py={30}>
       <Group sx={{ height: "100%" }} px={20} position="apart">
-        <Text>EmuAccom</Text>
+        <Anchor href="#">
+          <Text>EmuAccom</Text>
+        </Anchor>
+
+        <MediaQuery largerThan="sm" styles={{ display: "none" }}>
+          <Burger
+            opened={opened}
+            onClick={() => setOpened(o => !o)}
+            size="sm"
+            mr="xl"
+          />
+        </MediaQuery>
 
         <Group>
           {!isAuthenticated && (
@@ -41,9 +83,28 @@ export const Index = () => {
               <MoonStars size={16} />
             )}
           </ActionIcon>
+
+          <div className={classes.links}>
+            <MenuList />
+          </div>
         </Group>
       </Group>
     </Header>
+  );
+};
+
+export const NavBar = ({ opened }) => {
+  const { classes } = useStyles();
+
+  return (
+    <Navbar
+      fixed
+      className={classes.navbar}
+      width={{ base: "100%", sm: 0 }}
+      hidden={!opened}
+    >
+      <MenuList />
+    </Navbar>
   );
 };
 
