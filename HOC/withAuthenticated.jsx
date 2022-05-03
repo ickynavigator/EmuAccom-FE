082c6b91a97775c9e5079aaf1e7c7c77fcbb15eb
Obi Fortune <gabrielobi.of@gmail.com>
2022-05-03 15:37:56 +0300

    Updated the with auth wrapper

--- HOC/withAuthenticated.jsx
+++ HOC/withAuthenticated.jsx
@@ -1,40 +1,44 @@
 import { useRouter } from "next/router";
-import React, { useContext, useEffect, useState } from "react";
+import React, { useCallback, useContext, useEffect, useState } from "react";
+import { AUTHENTICATE_USER } from "../context/constants";
 import { store } from "../context/store";
 import { verifyToken } from "../utils/axiosRequests";
 
 const WithAuthenticated = Component => {
   const AuthenticatedComponent = () => {
-    const { state } = useContext(store);
-    const { user } = state;
-    const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const { state, dispatch } = useContext(store);
+    const Router = useRouter();
     const [isLoading, setIsLoading] = useState(true);
-    const router = useRouter();
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
 
-    useEffect(() => {
-      if (user?.token) {
-        const verifyTokenWrapper = async () => {
-          const res = await verifyToken(user.token);
-          if (res.status === 200 || res.status === 204 || res.status === 304) {
+    const verifyTokenWrapper = useCallback(async () => {
+      if (state.user) {
+        if (state.user.token) {
+          if (state.user.isAuthenticated) {
             setIsAuthenticated(true);
-          } else {
-            setIsAuthenticated(false);
-            router.push(`/login?redirect=${router.asPath}`);
+            setIsLoading(false);
+            return;
           }
-        };
 
-        verifyTokenWrapper();
-      } else {
-        setIsAuthenticated(false);
-        router.push(`/login?redirect=${router.asPath}`);
+          const res = await verifyToken(state.user.token);
+          if (res.status === 200 || res.status === 204 || res.status === 304) {
+            dispatch({ type: AUTHENTICATE_USER, payload: true });
+            setIsAuthenticated(true);
+            setIsLoading(false);
+            return;
+          }
+        }
       }
 
+      setIsAuthenticated(false);
+      dispatch({ type: AUTHENTICATE_USER, payload: false });
       setIsLoading(false);
-      return () => {
-        setIsAuthenticated(false);
-        setIsLoading(false);
-      };
-    }, [router, state.user, user]);
+      Router.push(`/login?redirect=${Router.asPath}`);
+    }, [state.user, dispatch, Router]);
+
+    useEffect(() => {
+      verifyTokenWrapper();
+    }, [verifyTokenWrapper]);
 
     return isLoading ? <>loading...</> : isAuthenticated && <Component />;
   };
--- context/constants.js
+++ context/constants.js
@@ -1,3 +1,5 @@
 // TYPES
 export const LOGIN_USER = "LOGIN_USER";
 export const LOGOUT_USER = "LOGOUT_USER";
+export const STORE_MANAGER = "STORE_MANAGER";
+export const AUTHENTICATE_USER = "AUTHENTICATE_USER";
