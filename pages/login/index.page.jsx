09f5db7eac174642178574acce6949b0dab9d744
Obi Fortune <gabrielobi.of@gmail.com>
2022-06-05 11:07:05 +0300

    added footer to login form and better error handling

--- pages/login/index.page.jsx
+++ pages/login/index.page.jsx
@@ -1,5 +1,15 @@
-import { Alert, Box, Button, Group, TextInput } from "@mantine/core";
+import {
+  Alert,
+  Anchor,
+  Box,
+  Button,
+  Container,
+  Divider,
+  Group,
+  TextInput,
+} from "@mantine/core";
 import { useForm } from "@mantine/form";
+import { NextLink } from "@mantine/next";
 import { useRouter } from "next/router";
 import React, { useContext, useEffect, useState } from "react";
 import { LOGIN_USER } from "../../context/constants";
@@ -13,6 +23,7 @@ const Index = () => {
   const { dispatch } = useContext(store);
   const router = useRouter();
   const [error, setError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
   useEffect(() => {
     if (isAuthenticated) {
       router.push("/");
@@ -36,7 +47,8 @@ const Index = () => {
   const submitHandler = async values => {
     setError(false);
     try {
-      const { data, status } = await signInRequest(values);
+      const res = await signInRequest(values);
+      const { data, status } = res;
       if (status === 200) {
         dispatch({ type: LOGIN_USER, payload: data });
         const returnUrl = router.query.redirect || "/";
@@ -45,47 +57,61 @@ const Index = () => {
       }
 
       setError(true);
+      if (res.response.status === 401) {
+        setErrorMessage(res.response.data.message);
+      }
+      setError(true);
     } catch (err) {
       setError(true);
     }
   };
 
   return (
-    <Box sx={{ maxWidth: 300 }} mx="auto">
-      {error && (
-        <Alert
-          title="Login Issue!"
-          color="red"
-          withCloseButton
-          closeButtonLabel="Close alert"
-          onClose={() => setError(false)}
-        >
-          Something terrible happened! Please try and login again.
-        </Alert>
-      )}
+    <Container>
+      <Box sx={{ maxWidth: 300 }} mx="auto">
+        {error && (
+          <Alert
+            title="Login Issue!"
+            color="red"
+            withCloseButton
+            closeButtonLabel="Close alert"
+            onClose={() => setError(false)}
+          >
+            {errorMessage ||
+              "Something terrible happened! Please try and login again."}
+          </Alert>
+        )}
+        <form onSubmit={formDetails.onSubmit(submitHandler)}>
+          <TextInput
+            required
+            label="Email"
+            placeholder="Enter your email"
+            type="email"
+            {...formDetails.getInputProps("email")}
+          />
+          <TextInput
+            required
+            label="Password"
+            placeholder="Enter your password"
+            type="password"
+            {...formDetails.getInputProps("password")}
+          />
+          <Group position="right" mt="md">
+            <Button type="submit">Submit</Button>
+          </Group>
+        </form>
+      </Box>
 
-      <form onSubmit={formDetails.onSubmit(submitHandler)}>
-        <TextInput
-          required
-          label="Email"
-          placeholder="Enter your email"
-          type="email"
-          {...formDetails.getInputProps("email")}
-        />
-
-        <TextInput
-          required
-          label="Password"
-          placeholder="Enter your password"
-          type="password"
-          {...formDetails.getInputProps("password")}
-        />
-
-        <Group position="right" mt="md">
-          <Button type="submit">Submit</Button>
-        </Group>
-      </form>
-    </Box>
+      <Group mt="md" position="center">
+        <Anchor component={NextLink} href="/signup">
+          Not Signed Up yet?
+        </Anchor>
+        <Divider sx={{ height: "24px" }} size="md" orientation="vertical" />
+        <Anchor component={NextLink} href="/login/forgot-password">
+          Forgot Password?
+        </Anchor>
+      </Group>
+    </Container>
   );
 };
 
