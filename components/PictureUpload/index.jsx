24a4c8863fa99144f1e8e401a5498bebbd911d66
Obi Fortune <gabrielobi.of@gmail.com>
2022-06-06 17:22:17 +0300

    Finished picture uploading

--- components/PictureUpload/index.jsx
+++ components/PictureUpload/index.jsx
@@ -1,18 +1,16 @@
+/// <reference path="../../types/typedefs.js" />
 import {
-  Button,
   Group,
   // eslint-disable-next-line no-unused-vars
   MantineTheme,
-  Text,
   useMantineTheme,
 } from "@mantine/core";
 import {
   Dropzone,
   // eslint-disable-next-line no-unused-vars
   DropzoneStatus,
-  IMAGE_MIME_TYPE,
 } from "@mantine/dropzone";
-import React, { useState } from "react";
+import React from "react";
 import { Photo, Upload, X } from "tabler-icons-react";
 import { Notifications as message } from "../../utils/Notifications";
 
@@ -46,7 +44,7 @@ const ImageUploadIcon = ({ status, ...props }) => {
   return <Photo {...props} />;
 };
 
-const dropzoneChildren = (status, theme) => (
+const dropzoneChildren = (status, theme, child) => (
   <Group
     position="center"
     spacing="xl"
@@ -58,26 +56,32 @@ const dropzoneChildren = (status, theme) => (
       size={80}
     />
 
-    <div>
-      <Text size="xl" inline>
-        Drag images here or click to select files
-      </Text>
-      <Text size="sm" color="dimmed" inline mt={7}>
-        Attach as many files as you like, each file should not exceed 5mb
-      </Text>
-    </div>
+    {child}
   </Group>
 );
 
+/**
+ * @param {{
+ *    files: File[],
+ *    setFiles: (files: File[]) => void,
+ *    onDrop?: (files: File[]) => void,
+ *    onDropRejected?: (fileRejects: FileRejection[]) => void,
+ *    child?: React.ReactNode,
+ *    FILE_TYPE: string[],
+ *    MAX_FILE_SIZE: number,
+ * }} props
+ */
 const Index = props => {
   const theme = useMantineTheme();
-  const MAX_FILE_SIZE = 3 * 1024 ** 2;
-  const { successCallBack, errorCallBack } = props;
-
-  const [files, setFiles] = useState(/** @type {File[]} */ []);
-  const [uploadedImage, setUploadedImage] = useState(
-    /** @type {{name: string, url: string}[]} */ [],
-  );
+  const {
+    files,
+    setFiles,
+    MAX_FILE_SIZE,
+    FILE_TYPE,
+    child,
+    onDrop,
+    onDropRejected,
+  } = props;
 
   /**
    * Verifies the File
@@ -91,19 +95,36 @@ const Index = props => {
       return false;
     }
 
-    // check if file is an image
-    if (!fileToCheck.type.match("image/")) {
-      message.error("File must be an image");
-      return false;
-    }
-
-    // check if file is larger than 1mb
-    if (fileToCheck.size > MAX_FILE_SIZE) {
-      message.error("File is larger than 1mb");
-      return false;
-    }
-
     return true;
+  }
+
+  /**
+   * Handles the file upload rejections
+   * @param {FileRejection[]} RejectedFiles
+   */
+  function handleFileUploadRejection(RejectedFiles) {
+    if (onDropRejected) {
+      onDropRejected(RejectedFiles);
+    }
+
+    if (RejectedFiles.length > 0) {
+      message.error("No file selected");
+    }
+
+    RejectedFiles.forEach(({ file, errors }) => {
+      let errStr = `For ${file.name}:`;
+
+      errors.forEach(err => {
+        if (err.code === "file-too-large") {
+          errStr += " File is too big. Max file size is 5MB.";
+        }
+        if (err.code === "file-invalid-type") {
+          errStr += " - File type not allowed.";
+        }
+      });
+
+      message.error(errStr);
+    });
   }
 
   /**
@@ -125,81 +146,21 @@ const Index = props => {
         setFiles([...files, file]);
       }
     });
-    // message.success("File selected successfully");
-  }
 
-  /**
-   * @param {File} fileToUpload
-   */
-  async function handleFileUpload(fileToUpload) {
-    const formData = new FormData();
-
-    formData.append("file", fileToUpload);
-    formData.append("upload_preset", process.env.NEXT_PUBLIC_CLOUDINARY_PRESET);
-
-    const options = { method: "POST", body: formData };
-
-    fetch(process.env.NEXT_PUBLIC_CLOUDINARY_URL, options)
-      .then(res => res.json())
-      .then(res => {
-        const { secure_url: secureUrl } = res;
-        if (secureUrl) {
-          setUploadedImage([
-            ...uploadedImage,
-            { ...fileToUpload, url: secureUrl },
-          ]);
-
-          message.success("Image Uploaded");
-
-          if (successCallBack) successCallBack(secureUrl);
-        } else {
-          message.error("An error has occured. Please try again");
-        }
-      })
-      .catch(err => {
-        message.error("An error has occured. Please try again");
-        if (errorCallBack) errorCallBack(err);
-      });
-  }
-
-  async function submitFiles() {
-    if (!files && files.length < 0) {
-      message.error("No file selected");
-      return;
+    if (onDrop) {
+      onDrop(fileList);
     }
-
-    files.forEach(curr => {
-      handleFileUpload(curr);
-    });
-  }
-
-  // eslint-disable-next-line no-unused-vars
-  async function parseImageBlob(preview) {
-    return fetch(preview)
-      .then(r => r.blob())
-      .then(
-        blobFile =>
-          new File([blobFile], new Date().toDateString(), {
-            type: preview.type,
-            ...preview,
-          }),
-      )
-      .then(res => res);
   }
 
   return (
-    <Group direction="column" align="center">
-      <Dropzone
-        onDrop={fileList => handleFileDrop(fileList)}
-        maxSize={3 * 1024 ** 2}
-        accept={IMAGE_MIME_TYPE}
-      >
-        {status => dropzoneChildren(status, theme)}
-      </Dropzone>
-      <Button align="right" onClick={() => submitFiles}>
-        Upload
-      </Button>
-    </Group>
+    <Dropzone
+      onDrop={FILES => handleFileDrop(FILES)}
+      onReject={FILES => handleFileUploadRejection(FILES)}
+      maxSize={MAX_FILE_SIZE}
+      accept={FILE_TYPE}
+    >
+      {status => dropzoneChildren(status, theme, child)}
+    </Dropzone>
   );
 };
 
